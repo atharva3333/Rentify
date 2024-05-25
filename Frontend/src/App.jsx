94a6fe156a555792dab2ab8e25d1b29decdaedd8
Atharva <scifacts001@gmail.com>
2024-05-25 12:55:02 +0530

    added landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Home from './components/Home'
+import LandingPage from './components/LandingPage'
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path='/register' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/' element={<Home />}/>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/home' element={<Home />}/>
       </Routes>
     </BrowserRouter>
   )
